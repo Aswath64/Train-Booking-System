2eea5dcf9e3fea6735e9a8454f26781a1c5e24be
Aswath64 <aswathmani64@gmail.com>
2024-11-13 21:48:34 +0530

    added

--- project2/src/login/LoginProvider.js
+++ project2/src/login/LoginProvider.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <LoginProvider> {/* Wrap the provider around the entire app */}
       <Router>
-        <Routes>
+        <Routes >
           <Route path="/" element={<Box1 />} />
           <Route path="/Sign" element={<Box2 />} />
           <Route path="/Home" element={<Home />} />
